a316f464e215c5651bd760f6e5d9a9c6a8b13d6f
TerryChen <ert4562007@gmail.com>
2024-09-02 05:08:19 +0800

    Second version Read airplan_temp at download folder. Call currency API.

---
+++ app/src/main/java/com/example/travelapp/model/MoneyCurrencyModel.kt
@@ -0,0 +1,13 @@
+package com.example.travelapp.model
+
+
+data class MoneyCurrency(
+    val KRW: String,
+    val AUD: String,
+    val EUR: String,
+    val CNY: String,
+    val JPY: String,
+    val USD: String
+)
+
+
